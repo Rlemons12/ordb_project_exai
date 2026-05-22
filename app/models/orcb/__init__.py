d503562a5fa30210b86723367119d0c4cb387ea0
Rlemons12 <kermitsrocket@gmail.com>
2026-05-22 16:12:55 -0500

    Remove OracleDB-related imports from ORCB module init file

--- app/models/orcb/__init__.py
+++ app/models/orcb/__init__.py
@@ -1,19 +1,11 @@
 from __future__ import annotations
 
-from app.models.orcb.oracle_db import (
-    OracleDatabaseRegistry,
-    OracleDBOperationResult,
-    OracleTableDefinition,
-    get_ai_request_audit_table_definition,
-    get_registered_oracle_tables,
-    render_oracle_identifier,
-)
+"""
+Oracle rebuild/registry package.
 
-__all__ = [
-    "OracleDatabaseRegistry",
-    "OracleDBOperationResult",
-    "OracleTableDefinition",
-    "get_ai_request_audit_table_definition",
-    "get_registered_oracle_tables",
-    "render_oracle_identifier",
-]
+Do not import app.models.orcb.oracle_db here.
+
+Use direct imports:
+
+    from app.models.orcb.oracle_db import OracleDatabaseRegistry
+"""
